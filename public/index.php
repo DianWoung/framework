56b17fb1ff721382d4168ee8992e4e8f0987393a
DianWoung <dian126@foxmail.com>
2019-04-01 12:32:05 +0800

    base helloworld finished

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use App\HelloWorld;
 use function DI\create;
+use function DI\get;
+use Zend\Diactoros\Response;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -12,9 +15,25 @@ $containerBuilder->useAutowiring(false);
 $containerBuilder->useAnnotations(false);
 $containerBuilder->addDefinitions([
     HelloWorld::class => create(HelloWorld::class)
+    ->constructor(get('Foo'), get('Response')),
+    'Foo' => 'bar',
+    'Response' => function() {
+        return new Response();
+    }
 ]);
 
 $container = $containerBuilder->build();
 
-$helloWorld = $container->get(HelloWorld::class);
-$helloWorld->announce();
+$routes = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $r->get('/hello', HelloWorld::class);
+});
+
+$middlewareQueue = [];
+$middlewareQueue[] = new \Middlewares\FastRoute($routes);
+$middlewareQueue[] = new \Middlewares\RequestHandler($container);
+$requestHandler = new \Relay\Relay($middlewareQueue);
+$response = $requestHandler->handle(\Zend\Diactoros\ServerRequestFactory::fromGlobals());
+
+$emitter = new SapiEmitter();
+$emitter->emit($response);
+
--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -3,10 +3,27 @@ declare(strict_types=1);
 
 namespace App;
 
+use Psr\Http\Message\ResponseInterface;
+
 class HelloWorld
 {
-    public function announce(): void
+    private $foo;
+
+    private $response;
+
+    public function __construct(string $foo,
+        ResponseInterface $response)
     {
-        echo 'Hello, autoloaded world';
+        $this->foo = $foo;
+        $this->response = $response;
+    }
+
+    public function __invoke(): ResponseInterface
+    {
+      $response = $this->response->withHeader('Content-Type', 'text/html');
+      $response->getBody()
+          ->write("<html><head></head><body>Hello, {$this->foo} world!</body></html>");
+
+      return $response;
     }
 }
